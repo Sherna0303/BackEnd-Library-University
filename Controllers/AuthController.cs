e9ef2acc323bcc6acefb21a7dcdc163f45dd896a
Sebastian Hernandez <sebastianhernandezpatio.2004@gmail.com>
2024-10-25 18:40:56 -0500

    Add endpoint to register user in authentication controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using LibrarySystemWeb.Interfaces;
 using LibrarySystemWeb.Models.Dtos;
+using LibrarySystemWeb.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,11 +11,13 @@ namespace LibrarySystemWeb.Controllers
     public class AuthController : ControllerBase
     {
         private readonly ILoginService _loginService;
+        private readonly IRegisterService _registerService;
         private readonly ICreateTokenService _createToken;
 
-        public AuthController( ILoginService loginService, ICreateTokenService createToken )
+        public AuthController( ILoginService loginService, IRegisterService registerService, ICreateTokenService createToken )
         {
             _loginService = loginService;
+            _registerService = registerService;
             _createToken = createToken;
         }
 
@@ -30,6 +33,21 @@ namespace LibrarySystemWeb.Controllers
             }
 
             string jwtToken = await _createToken.GenerateToken( userDto );
+
+            return Ok( new { token = jwtToken } );
+        }
+
+        [HttpPost( "register" )]
+        public async Task<IActionResult> Register( UserRegisterDto userRegisterDTO )
+        {
+            var user = await _registerService.RegisterUser( userRegisterDTO );
+
+            if ( user is null )
+            {
+                return BadRequest( new { message = "Error" } );
+            }
+
+            string jwtToken = await _createToken.GenerateToken( new UserDto() { Email = user.Email, Password = user.Password } );
 
             return Ok( new { token = jwtToken } );
         }
